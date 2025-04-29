49848ca48c72177db749c4a2fea04d0e04d1ea64
Maniteja0126 <maniteja2601@gmail.com>
2025-04-29 22:41:23 +0530

    Remove dist folder from repository

--- dist/config/db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const globalForPrisma = globalThis;
-const prisma = (_a = globalForPrisma.prisma) !== null && _a !== void 0 ? _a : new client_1.PrismaClient();
-exports.default = prisma;
