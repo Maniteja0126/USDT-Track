75ebbaea938447a1c8bd9d14ffbfc82b5fe291e1
Maniteja <135437049+Maniteja0126@users.noreply.github.com>
2025-08-06 17:22:28 +0530

    Update redis.ts

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,7 +1,7 @@
 import {createClient} from "redis";
 
 const redisClient = createClient({
-    url: "redis://localhost:6379",
+    url: "REDIS_URL=redis://127.0.0.1:6379",
 });
 
 redisClient.connect();
@@ -19,3 +19,4 @@ redisClient.on("connect", () => {
 
 
 export default redisClient;
+
